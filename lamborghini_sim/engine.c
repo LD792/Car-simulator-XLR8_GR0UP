f3ac4e378f2ed11960d9e0d42f10868195f48c48
LD792 <mednihs2@gmail.com>
2025-07-03 16:16:44 +0100

    Update engine logic and other project files

--- lamborghini_sim/engine.c
+++ lamborghini_sim/engine.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <errno.h> 
 #include <ctype.h>
@@ -7,11 +8,53 @@
 #include "include/engine_data.h"
 
 
-get_torque(const TorqueEntry* table, unsigned int curr_rpm){
-    for(int i = 0; i < LAMBO_TORQUE_TABLE_SIZE - 1; i++){
+float get_torque(const TorqueEntry* table, unsigned int rpm){
 
+    if (rpm < idle_rpm)
+        return 0.0;
 
+    //this implementation is just for begining after inchaallah i will add more realistic behavior
+    if (rpm > rev_limiter)
+        rpm = rev_limiter;
+
+    if ((rpm - idle_rpm)%50 == 0){
+        return table[(rpm - idle_rpm)/50].torque;
     }
+    else
+    {
+        int lower_rpm = rpm - (rpm % 50);    
+        int upper_rpm = lower_rpm + 50;
+        int lower_index = (lower_rpm - idle_rpm) / 50;
+        int upper_index = lower_index + 1;
+        float torque_lower = table[lower_index].torque;
+        float torque_upper = table[upper_index].torque;
+        float t = (rpm - lower_rpm) / 50.0;
+        float torque = torque_lower + (torque_upper - torque_lower) * t;
+        return torque; 
+    }
+
+}
+
+
+float angular_acc(float engine_torque){
+
+    return engine_torque / engine_inertia;
+
+}
+
+float angular_velocity(float angular_acceleration, float delta_time){
+
+    return angular_acceleration * delta_time;
 
 
 }
+
+unsigned int get_engine_rpm(float angular_velocity){
+
+    return (unsigned int) round( angular_velocity * 30 / M_PI);
+
+
+}
+
+// next to do inchaallah: Gearbox logique 
+
--- lamborghini_sim/engine.h
+++ lamborghini_sim/engine.h
@@ -2,6 +2,9 @@
 #define ENGINE_H
 
 extern const unsigned int idle_rpm = 800;
+extern const unsigned int max_rpm = 9000;
+extern const unsigned int rev_limiter = 8950;
+extern const float engine_inertia = 0.85;
 extern const float max_throtlle = 1.0;
 
 #endif 
